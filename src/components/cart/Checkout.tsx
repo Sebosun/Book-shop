d5f339eac218c78f66ad53846b2c8b0817678f66
Sebosun <sebos@protonmail.com>
2021-12-28 23:36:46 +0100

    POST request on form submit

--- src/components/cart/Checkout.tsx
+++ src/components/cart/Checkout.tsx
@@ -1,11 +1,39 @@
 import type { ReactElement } from "react";
+import { useAppSelector } from "../../store/app/hooks";
 import CheckoutForm from "./CheckoutForm";
 
+export interface customerData {
+  first_name: string;
+  last_name: string;
+  city: string;
+  zip_code: string;
+}
+
 export default function Checkout(): ReactElement | null {
+  const { cart } = useAppSelector((state) => state.cart);
+  const handleOrderConfirm = async (data: customerData) => {
+    const purchaseData = cart.map((val) => {
+      return { id: val.id, quantity: val.quantity };
+    });
+
+    try {
+      const post = await fetch("http://localhost:3001/api/order", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ order: purchaseData, ...data }),
+      });
+      console.log(post);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <div className="max-w-md mx-auto">
-        <CheckoutForm />
+        <CheckoutForm handleOrderConfirm={handleOrderConfirm} />
       </div>
     </>
   );
