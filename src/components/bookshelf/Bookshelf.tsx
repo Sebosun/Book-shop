fe8896570b9be821f60b6205da7c47b5084e9787
Sebosun <sebos@protonmail.com>
2021-12-28 16:55:35 +0100

    further stylization of books

--- src/components/bookshelf/Bookshelf.tsx
+++ src/components/bookshelf/Bookshelf.tsx
@@ -14,14 +14,23 @@ export default function Bookshelf({
   pageNumber,
 }: bookTypes): ReactElement | null {
   return (
-    <div className="flex flex-col max-w-ms">
+    <div className="grid grid-cols-2 gap-4 p-4 rounded-lg border-2 border-solid shadow-xl max-w-ms">
       <img className="object-contain h-96" src={cover_url} alt="book cover" />
-      <div>{title}</div>
-      <div>{author}</div>
-      <div>{pageNumber}</div>
-      <button className="p-4 text-white text-lg rounded-xl w-48 self-center bg-gradient-to-r from-cyan-500 to-blue-500 ">
-        Dodaj do koszyka
-      </button>
+      <div className="flex flex-col justify-evenly text-left">
+        <div className="text-gray-500">
+          Tytul: <span className="text-gray-900">{title}</span>
+        </div>
+        <div className="text-gray-600">
+          Autor: <span className="text-gray-900">{author}</span>
+        </div>
+        <div className="text-gray-600">
+          <span className="text-gray-900">{pageNumber}</span> stron
+        </div>
+
+        <button className="p-4 justify-self-end w-48 text-lg text-white bg-gradient-to-r from-cyan-500 to-blue-500 rounded-xl border-2 border-sky-100 shadow-md">
+          Dodaj do koszyka
+        </button>
+      </div>
     </div>
   );
 }
