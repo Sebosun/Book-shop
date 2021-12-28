67c278d5deabfacb1691789e7bcbe8c0ff706361
Sebosun <sebos@protonmail.com>
2021-12-28 18:30:16 +0100

    calculating carts lenght

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,16 @@
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { useAppSelector } from "../../store/app/hooks";
 
 export default function Header(): ReactElement | null {
+  const cart = useAppSelector((state) => state.cart);
+
+  const calcCartLength = () => {
+    return cart.cart.reduce((prev, cur) => {
+      return prev + cur.quantity;
+    }, 0);
+  };
   return (
     <div className="p-2 text-2xl border-b-2 border-solid border-sky-300">
       <nav className="flex items-center justify-between mx-24">
@@ -12,7 +20,7 @@ export default function Header(): ReactElement | null {
           className="flex justify-center text-2xl items-center btn-primary"
         >
           <AiOutlineShoppingCart />
-          <p>Koszyk ({0})</p>
+          <p>Koszyk ({calcCartLength()})</p>
         </Link>
       </nav>
     </div>
