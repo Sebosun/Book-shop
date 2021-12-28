bb566dc19cf2b517913f273f63bb9f8a96e0e4a5
Sebosun <sebos@protonmail.com>
2021-12-28 13:55:55 +0100

    styling + prep for incoming data

--- src/components/bookshelf/Bookshelf.tsx
+++ src/components/bookshelf/Bookshelf.tsx
@@ -1,19 +1,24 @@
 import React, { ReactElement } from "react";
 
 interface bookTypes {
-  imageSrc: string;
+  cover_url: string;
   title: string;
   author: string;
   pageNumber: number;
 }
 
-export default function Bookshelf(): ReactElement | null {
+export default function Bookshelf({
+  cover_url,
+  title,
+  author,
+  pageNumber,
+}: bookTypes): ReactElement | null {
   return (
     <div className="flex flex-col max-w-xs">
-      <img src="https://i.imgur.com/nuPBO0I.jpeg" />
-      <div>Title</div>
-      <div>Author</div>
-      <div>Page no.</div>
+      <img className="object-contain h-96" src={cover_url} />
+      <div>{title}</div>
+      <div>{author}</div>
+      <div>{pageNumber}</div>
       <button>Dodaj do koszyka</button>
     </div>
   );
