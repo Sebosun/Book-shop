8a7236fb9888e75a1d1ae105a22d69bcb7ded15e
Sebosun <sebos@protonmail.com>
2021-12-28 17:12:02 +0100

    links on header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,17 @@
 import { ReactElement } from "react";
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 export default function Header(): ReactElement | null {
   return (
-    <div className="flex justify-end items-center  p-2 border-b-2 border-solid border-sky-300">
-      <button>Koszyk</button>
-      <AiOutlineShoppingCart />
+    <div className="p-2 text-2xl border-b-2 border-solid border-sky-300">
+      <nav className="flex justify-between mx-24">
+        <Link to="/">Home</Link>
+        <Link to="/cart" className="flex items-center">
+          <AiOutlineShoppingCart />
+          <p>Koszyk</p>
+        </Link>
+      </nav>
     </div>
   );
 }
