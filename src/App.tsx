d8ec7ff0552988edab9a70a548dc991ec0485cb1
Sebosun <sebos@protonmail.com>
2021-12-28 13:10:19 +0100

    basic fetch of books

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import "./App.css";
 
 function App() {
+  const fetchBooks = async () => {
+    // TODO error handling
+    let request = await fetch("http://localhost:3001/api/book");
+    let reader = request.body?.getReader();
+    let readReader = await reader?.read();
+    console.log(JSON.parse(new TextDecoder().decode(readReader?.value)));
+  };
+
   return <div className="App"></div>;
 }
 
