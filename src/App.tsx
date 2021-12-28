e54fa53727cbbc2c06ad3850eb33dc061d02af6f
Sebosun <sebos@protonmail.com>
2021-12-28 23:30:46 +0100

    v. simple 404 and conditional on checkout

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,19 @@ import Cart from "./components/cart/Cart";
 import Checkout from "./components/cart/Checkout";
 import Header from "./components/header/Header";
 import Main from "./components/pages/Main";
+import { useAppSelector } from "./store/app/hooks";
 
 function App() {
+  const { cart } = useAppSelector((state) => state.cart);
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
         <Routes>
+          <Route path="*" element={<p>404 - Not found</p>} />
           <Route path="/" element={<Main />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
+          {cart.length > 0 && <Route path="/checkout" element={<Checkout />} />}
         </Routes>
       </BrowserRouter>
     </div>
