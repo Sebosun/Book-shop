51c726a65e8deaf755996fc1ee44765479830706
Sebosun <sebos@protonmail.com>
2021-12-30 00:31:54 +0100

    testing for Main component and refurbishing fetching logic

---
+++ src/components/main/Main.test.js
@@ -0,0 +1,46 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { createStore } from "../../store/app/store";
+import Main from "./Main";
+
+test("renders after succesful api call", async () => {
+  const books = [
+    {
+      id: 41,
+      title: "Taejtet",
+      author: "Plato",
+      cover_url: "./:).jpg",
+      pages: 150,
+      price: 15000,
+      currency: "PLN",
+    },
+  ];
+
+  const metadata = {
+    page: 1,
+    records_per_page: 1,
+    total_records: 1,
+  };
+
+  const dataTogether = { data: books, metadata };
+
+  window.fetch = jest.fn();
+
+  window.fetch.mockResolvedValueOnce({
+    json: async () => dataTogether,
+  });
+
+  render(
+    <Provider store={createStore()}>
+      <Main />
+    </Provider>
+  );
+
+  const author = await screen.findByText(/Plato/i);
+  const title = await screen.findByText(/Taejtet/i);
+  const pages = await screen.findByText(/150/i);
+
+  expect(author).toBeInTheDocument();
+  expect(title).toBeInTheDocument();
+  expect(pages).toBeInTheDocument();
+});
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,5 +1,4 @@
 import { ReactElement, useEffect, useState } from "react";
-import fetchBooks from "../../helpers/fetchBooks";
 import Bookshelf from "./Bookshelf";
 
 //TODO: export types to separate compoents ?
@@ -13,28 +12,46 @@ export interface bookTypes {
   currency: string;
 }
 
-export default function Main(): ReactElement | null {
-  const [books, setBooks] = useState<bookTypes[]>([]);
+interface fetchData {
+  data: bookTypes[];
+  metadata: {
+    page: number;
+    records_per_page: number;
+    total_records: number;
+  };
+}
 
-  const helperFetch = async () => {
-    const fetch = await fetchBooks();
-    setBooks(fetch.data);
+export default function Main(): ReactElement | null {
+  const [books, setBooks] = useState<fetchData | false>(false);
+
+  const fetchBooks = async () => {
+    try {
+      const request = await fetch("http://localhost:3001/api/book");
+      const parsed = await request.json();
+      setBooks(parsed);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   // run fetchBooks when the component loads
   useEffect(() => {
-    helperFetch();
+    fetchBooks();
   }, []);
 
-  return (
-    <>
-      {books && (
-        <div className="grid gap-4 xl:gap-6 my-6 grid-row-1 xl:grid-cols-2 mx-2 md:mx-14 2xl:grid-cols-3">
-          {books.map((book) => {
-            return <Bookshelf key={book.id} book={book} />;
-          })}
-        </div>
-      )}
-    </>
-  );
+  if (books) {
+    return (
+      <div>
+        {books && (
+          <div className="grid gap-4 xl:gap-6 my-6 grid-row-1 xl:grid-cols-2 mx-2 md:mx-14 2xl:grid-cols-3">
+            {books.data.map((book) => {
+              return <Bookshelf key={book.id} book={book} />;
+            })}
+          </div>
+        )}
+      </div>
+    );
+  } else {
+    return <p>Loading</p>;
+  }
 }
