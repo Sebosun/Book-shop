5cae1916859e04f474cec609d2ea110af2d37fb3
Sebosun <sebos@protonmail.com>
2021-12-30 04:09:13 +0100

    mobile header improvements

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -9,11 +9,13 @@ export default function Header(): ReactElement | null {
 
   return (
     <div className="p-2 text-2xl border-b-2 border-solid border-sky-300">
-      <nav className="flex items-center justify-between mx-24">
-        <Link to="/">Home</Link>
+      <nav className="flex items-center justify-between mx-2 md:mx-24">
+        <Link className="p-4 hover:bg-sky-200 rounded-md" to="/">
+          Home
+        </Link>
         <Link
           to="/cart"
-          className="flex justify-center text-2xl items-center btn-primary"
+          className="flex justify-center md:text-2xl items-center btn-primary"
         >
           <AiOutlineShoppingCart />
           <p>Koszyk ({calcCartLenght(cart)})</p>
